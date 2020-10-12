70848e0cce819931a45543892e6848c3e3428852
szavalia <szavalia@itba.edu.ar>
2020-10-12 20:21:53 -0300

    Me como una coma

--- proyectoweb/src/components/Menu.vue
+++ proyectoweb/src/components/Menu.vue
@@ -119,7 +119,7 @@ export default {
           text2:"ONG",
           text3:"Quiero hacerme visible para gente que quiera ayudarnos."
         }
-      ]
+      ],
       aboutLink: '/about',
       homeLink: '/'
     }
