eda0da2e8ad5df70faf140deee2c055bc29742ae
irevich <irevich@itba.edu.ar>
2020-12-08 02:15:38 -0300

    Mapa centrado en direccion del usuario

--- proyectoweb/src/views/MapView.vue
+++ proyectoweb/src/views/MapView.vue
@@ -53,6 +53,8 @@ export default {
   data() {
     return {
       map: '',
+      currentUser : {},
+      userStore: UserStore,
       markers: [],
       descriptions: [
         // {
@@ -121,15 +123,16 @@ export default {
          });
       }
     },
-
-  // TODO: CENTRAR EN DIRECCIÓN DEL USUARIO
+  
   async mounted() {
+    this.currentUser = await this.userStore.getCurrentUser();
     let mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
     mapboxgl.accessToken = 'pk.eyJ1IjoiYW5pdGFjcnV6IiwiYSI6ImNrZ3A5d2Z6ZDA3M3Iyc2tsbmJjeGd4N2EifQ.7XEbgzqidyEuJKVfFg4U2A';
     this.map = new mapboxgl.Map({
       container: 'map', //the id of an element on your page where you would like your map.
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-58.3876273, -34.5903130],
+      // center: [-58.3876273, -34.5903130],
+      center : [this.currentUser.longitude, this.currentUser.latitude],
       zoom: 14
     });
     const nav = new mapboxgl.NavigationControl();
@@ -142,10 +145,22 @@ export default {
     const campaigns = answer.results;
     console.log(campaigns);
     // let ong;
+
     for( let i=0 ; i < campaigns.length ; i++){
+      //let titleBackup = this.descriptions[i].title;
+      // console.log(this.descriptions[i].title);
+      // this.descriptions[i].title.concat("\n");
       this.descriptions[i].marker = new mapboxgl.Marker()
             .setLngLat(await geomapApi.getCoordinates(new AddressData(campaigns[i].street,campaigns[i].street_number, campaigns[i].city)))
+            .setPopup(new mapboxgl.Popup().setText(this.descriptions[i].title+'\n'+this.descriptions[i].ong+'\n'
+            +this.descriptions[i].direccion+'\n'+this.descriptions[i].lista))
+            // .setPopup(new mapboxgl.Popup().setTitle(this.descriptions[i].title).setText(this.descriptions[i].ong+'\n'
+            //      +this.descriptions[i].direccion+'\n'+this.descriptions[i].lista))
+            // .setPopup(new mapboxgl.Popup().setHTML("<h1>{{this.descriptions[i].title}}</h1>" +
+            //        "<p>{{this.descriptions[i].ong}}</p>" +
+            //        "<p>{{this.descriptions[i].direccion}}</p>"))
             .addTo(this.map);
+      //this.descriptions[i].title = titleBackup;
      }
 
     }
