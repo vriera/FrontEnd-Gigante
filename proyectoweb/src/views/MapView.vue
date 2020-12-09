18a3405d0cb49b0932c015aa4e720eabc443a9d5
Luciana Diaz Kralj <lucidiaz@itba.edu.ar>
2020-12-09 11:39:00 -0300

    Mapa con filtros (Revisar bug en los tags)
    Filtra bien pero los tags de la tarjeta son los de la campaña ubicada en esa posición en el array anterior. Si se scrollea se arregla

--- proyectoweb/src/views/MapView.vue
+++ proyectoweb/src/views/MapView.vue
@@ -12,38 +12,27 @@
                 <v-icon>{{ filtros ? 'mdi-filter-variant-remove':'mdi-filter-variant' }}</v-icon>
               </v-btn>
             </template>
-            <span>Filtrar</span>
+            <span>{{ filtros ? 'Ocultar filtros':'Ver filtros' }}</span>
           </v-tooltip>
         </v-card-title>
-        <v-card-text v-if="filtros" v-model="selection">
+        <v-card-text v-if="filtros">
             <v-chip-group active-class="primary--text" column multiple>
-              <v-chip v-for="tag in categories" :key="tag.id_category">{{ tag.description }}</v-chip>
+              <v-chip v-for="tag in categories" :key="tag.id_category" v-on:click="addCat(tag.id_category)">{{ tag.description }}</v-chip>
             </v-chip-group>
-            <v-card-actions>
+            <!-- <v-card-actions>
               <v-spacer/>
               <v-btn text color="blue" v-on:click="filter()">Aplicar filtros</v-btn>
-            </v-card-actions>
+            </v-card-actions> -->
         </v-card-text>
 
-        <v-virtual-scroll height="700px"  item-height="270px" :items="filteredList">
+        <v-layout v-if="!loaded" column align-center class="py-5">
+          <v-progress-circular indeterminate color="blue"/>
+        </v-layout>
+
+        <v-virtual-scroll height="700px"  item-height="285px" :items="filteredList" v-if="loaded">
           <template v-slot:default="{ item }">
-            <v-card v-on:click="pan(item.lat_lng); item.marker.togglePopup();" class="mx-2" color="#DBEDF8" height="250">
-                <v-card-title >{{item.title}}</v-card-title>
-                <v-card-subtitle>{{ item.ong}}</v-card-subtitle>
-                <v-card-text>
-                  <v-layout column>
-                    <v-flex><v-icon small color="#72BAE6">mdi-map-marker sm</v-icon> {{item.direccion}}</v-flex>
-                    <v-flex><v-icon small class="my-3" color="#72BAE6">mdi-clock</v-icon> {{item.horario}}</v-flex>
-                    <v-flex class="d-inline-block text-truncate"><v-icon small color="#72BAE6">mdi-format-list-numbered</v-icon> {{item.lista}}</v-flex>
-                  </v-layout>
-                </v-card-text>
-                <!-- TODO: Categorías -->
-
-
-                <v-card-actions>
-                  <v-spacer/>
-                  <v-btn dark color="blue" :to="'/VerCampaña/' + item.camp_id">Más información</v-btn>
-                </v-card-actions>
+            <v-card flat v-on:click="pan(item.lat_lng); item.marker.togglePopup();">
+              <map-item :camp="item.obj" :ong="item.ong" :cat_list="item.cat_camp"/>
             </v-card>
           </template>
         </v-virtual-scroll>
@@ -63,8 +52,10 @@ import {geomapApi, AddressData} from '@/Geocode/geomap.js'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import CampaignStore from "@/store/CampaignStore";
 import UserStore from "@/store/UserStore";
+import MapItem from '../components/mapItem.vue';
 
 export default {
+  components: { MapItem },
   data() {
     return {
       map: '',
@@ -73,28 +64,42 @@ export default {
       markers: [],
       descriptions: [],
       filtros: false,
+      loaded: false,
       selection: [],
       categories: []
     }
   },
   computed: {
     filteredList() {
-      // if(this.selection.length < 1) {
+      if(this.selection.length > 0) {
+        return this.descriptions.filter(desc => {
+          for(let i=0; i<desc.cat_camp.length; i++) {
+            console.log("COMPARING "+this.selection+" WITH "+desc.cat_camp[i]);
+            if(this.selection.includes(desc.cat_camp[i]))
+              return true;
+          }
+          return false;
+        });
+      }
         return this.descriptions;
-      // }
-      // return this.descriptions.filter(camp => {
-      //   const ans = CampaignStore.getCampaignCategories(camp.camp_id);
-      //   console.log(ans);
-      //   return;
-      // });
     }
   },
   methods: {
     filter() {
-      this.filtros = false;
+      // this.search = true;
+      console.log("SELECTED FILTERS: "+this.selection);
     },
     pan(coord) {
       this.map.panTo(coord);
+    },
+    addCat(cat_id) {
+      if(!this.selection.includes(cat_id)) {
+          this.selection.push(cat_id);
+      } else {
+        var index = this.selection.indexOf(cat_id);
+        if (index > -1)
+          this.selection.splice(index, 1);
+      }
     }
   },
 
@@ -103,21 +108,22 @@ export default {
     const campaigns = answer.results;
     const ans = await CampaignStore.getCategories();
     this.categories = ans.results;
-    console.log(campaigns);
+    // console.log(campaigns);
     let ong;
     for( let i=0 ; i < campaigns.length ; i++){
         ong = await UserStore.getOngs(campaigns[i].id_ong);
         this.descriptions.push({
-          title : campaigns[i].name,
-          ong : ong.fullname,
-          direccion : campaigns[i].street+' '+campaigns[i].street_number+' '+campaigns[i].city,
-          horario : campaigns[i].schedule,
+          obj: campaigns[i],
+          ong: ong.fullname,
+          title: campaigns[i].name,
+          direccion : campaigns[i].street+' '+campaigns[i].street_number+', '+campaigns[i].location+', '+campaigns[i].city,
           lista : campaigns[i].description,
-          camp_id: campaigns[i].id_campaign,
           lat_lng: {},
-          marker : {}
+          cat_camp: [],
+          marker: {}
          });
       }
+      this.loaded = true;
     },
   
   async mounted() {
@@ -136,12 +142,9 @@ export default {
 
     const answer = await CampaignStore.getActiveCampaigns();
     const campaigns = answer.results;
-    console.log(campaigns);
+    // console.log(campaigns);
 
     for( let i=0 ; i < campaigns.length ; i++){
-      //let titleBackup = this.descriptions[i].title;
-      // console.log(this.descriptions[i].title);
-      // this.descriptions[i].title.concat("\n");
       const latLong = await geomapApi.getCoordinates(new AddressData(campaigns[i].street,campaigns[i].street_number, campaigns[i].city));
       this.descriptions[i].lat_lng = latLong;
       this.descriptions[i].marker = new mapboxgl.Marker()
@@ -149,7 +152,16 @@ export default {
             .setPopup(new mapboxgl.Popup().setHTML('<h3>'+this.descriptions[i].title+'</h3><h4>'+this.descriptions[i].ong+'</h4><h5>'
             +this.descriptions[i].direccion+'</h5><p>'+this.descriptions[i].lista+'</p>'))
             .addTo(this.map);
-      //this.descriptions[i].title = titleBackup;
+
+      const campCat = await CampaignStore.getCampaignCategories(this.descriptions[i].obj.id_campaign);
+      const aux = [];
+      // console.log(this.descriptions[i].title);
+      for(let i=0; i<campCat.results.length; i++) {
+        // console.log(campCat.results[i].id_category);
+        aux[i] = campCat.results[i].id_category;
+      }
+      this.descriptions[i].cat_camp = aux;
+      // console.log(this.descriptions[i].title+" ARRAY: "+this.descriptions[i].cat_camp+" ----");
     }
   }
 }
